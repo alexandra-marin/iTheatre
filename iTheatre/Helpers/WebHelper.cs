7e5db6fea265955c131b4299b028ed61ed289053
Alexandra Marin <alexandra.marin01@gmail.com>
2016-12-23 17:52:01 +0000

    Move web requests into own class

---
+++ iTheatre/Helpers/WebHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace iTheatre
+{
+	public class WebHelper
+	{
+		private HttpClient client = new HttpClient();
+
+		public async Task<string> Get(string address)
+		{
+			using (HttpResponseMessage response = await client.GetAsync(address))
+			using (HttpContent content = response.Content)
+			{
+				return await content.ReadAsStringAsync();
+			}
+		}
+	}
+}
--- iTheatre/Services/MoviesAPI.cs
+++ iTheatre/Services/MoviesAPI.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -7,42 +6,25 @@ namespace iTheatre
 {
 	public class MoviesAPI : IMoviesAPI
 	{
-		private HttpClient client;
-
 		private static readonly string baseAddress = "https://api.themoviedb.org/3/";
 		private static readonly string moviePath = "movie/";
 		private static readonly string movieCastPath = "/credits";
 		private static readonly string apiKey = string.Format("?api_key={0}", Credentials.MDBIKey);
 
-		public MoviesAPI()
-		{
-			client = new HttpClient();
-		}
+		private WebHelper client = new WebHelper();
 
 		public async Task<Movie> GetMovie(string movieId)
 		{
-			string json;
 			var address = baseAddress + moviePath + movieId + apiKey;
-
-			using (HttpResponseMessage response = await client.GetAsync(address))
-			using (HttpContent content = response.Content)
-			{
-				json = await content.ReadAsStringAsync();
-			}
+			var json = await client.Get(address);
 
 			return JsonConvert.DeserializeObject<Movie>(json);
 		}
 
 		public async Task<List<Actor>> GetMovieCast(string movieId)
 		{
-			string json;
 			var address = baseAddress + moviePath + movieId + movieCastPath + apiKey;
-
-			using (HttpResponseMessage response = await client.GetAsync(address))
-			using (HttpContent content = response.Content)
-			{
-				json = await content.ReadAsStringAsync();
-			}
+			var json = await client.Get(address);
 
 			var definition = new { Cast = new List<Actor>() };
 			var credits = JsonConvert.DeserializeAnonymousType(json, definition);
