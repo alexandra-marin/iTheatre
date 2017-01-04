d630d570c9e80b8f43303e1798d5faa47a7fd03d
Alexandra Marin <alexandra.marin01@gmail.com>
2017-01-04 00:23:49 +0000

    Increase web helper timeout

--- iTheatre/Helpers/WebHelper.cs
+++ iTheatre/Helpers/WebHelper.cs
@@ -1,11 +1,12 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace iTheatre
 {
 	public class WebHelper
 	{
-		private HttpClient client = new HttpClient();
+		private HttpClient client = new HttpClient() { Timeout = TimeSpan.FromMinutes(5)};
 
 		public async Task<string> Get(string address)
 		{
